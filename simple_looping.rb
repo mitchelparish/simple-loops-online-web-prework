ef3e81893b3beecefb71b5290332c292bb0e3582
Mitchel Parish <mitchelparish@gmail.com>
2018-12-27 20:34:34 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -47,8 +47,12 @@ def until_iterator(number_of_times)
 end
 
 def for_iterator(number_of_times)
-
   phrase = "Welcome to Flatiron School's Web Development Course!"
+  for number_of_times in 1..40 do
+    puts phrase
+  end
+  end
+
 
 
 
